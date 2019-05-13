02f1a77ef210b6f5245a0d46f03e7ae885b3d2e3
Alan Nascimento <alan.nascimento@discover.com.br>
2019-05-13 16:46:14 -0300

    Adding scss loader

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { TaskController } from './ts/controllers/index';
+import './scss/smartlist.scss';
 
 const controller = new TaskController();
 document.getElementById('task-form').onsubmit = controller.add.bind(controller);
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,14 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
             }
         ]
     },

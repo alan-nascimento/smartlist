058c40cf3df3ff0ea30255d6de2899860ba6cf75
Alan Nascimento <alan.nascimento@discover.com.br>
2019-05-14 17:06:16 -0300

    Adding layout styles

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,10 @@ import './scss/smartlist.scss';
 
 const controller = new TaskController();
 document.getElementById('task-form').onsubmit = controller.add.bind(controller);
+
+const form = document.querySelector('.task-form')
+
+const add = document.querySelector('btn-add');
+add.addEventListener('click', () => {
+  form.classList.toggle('');
+});
